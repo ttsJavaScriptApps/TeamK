8a7079771d8b2a2b48112b95905d69560b6dcc50
TzolkinB <krbell4@gmail.com>
2016-01-21 01:40:54 -0500

    Loop still infinite

--- rps.js
+++ rps.js
@@ -1,47 +1,45 @@
-
-var userChoice = parseInt(Math.random()*10)%3;
-var computerChoice = parseInt(Math.random()*10)%3;
-
 var userScore = 0
 var computerScore = 0
 
-if (userChoice = 0) {
-  	userChoice = "rock";
-} 
-else if(userChoice = 1) {
-  	userChoice = "paper";
-} 
-else {
-  	userChoice = "scissors";
-} 
+var userChoice = parseInt(Math.random()*10)%3;
+var computerChoice = parseInt(Math.random()*10)%3;
+  
+  if (userChoice = 0) {
+      userChoice = "rock";
+  } 
+  else if(userChoice = 1) {
+      userChoice = "paper";
+  } 
+  else {
+      userChoice = "scissors";
+  } 
 
-if (computerChoice == 0) {
-    computerChoice = "rock";
-} 
-else if(computerChoice == 1) {
-    computerChoice = "paper";
-} 
-else {
-    computerChoice = "scissors";
-} 
+  if (computerChoice == 0) {
+      computerChoice = "rock";
+  } 
+  else if(computerChoice == 1) {
+      computerChoice = "paper";
+  } 
+  else {
+      computerChoice = "scissors";
+  } 
 
 var compare = function(choice1, choice2) {
-    
+      
   if (choice1 === choice2) {
       return "The result is a tie!";
   }
-    
+      
   else if (choice1 === "rock") {
     if (choice2 === "scissors") {
       return "rock wins";
       userScore += 1;
     }  
     else { 
-        return "paper wins";
-        computerScore += 1;
+      return "paper wins";
+      computerScore += 1; 
     }
-  }
-    
+  }   
   else if(choice1 === "paper") {
     if (choice2 === "rock") {
        return "paper wins";
@@ -49,9 +47,9 @@ var compare = function(choice1, choice2) {
     }
     else { 
         return "scissors wins";
-    }   computerScore += 1; 
-  }
-    
+        computerScore += 1; 
+    }
+  }    
   else if(choice1 === "scissors") {
     if (choice2 === "rock"){ 
         return "rock wins";
@@ -62,14 +60,12 @@ var compare = function(choice1, choice2) {
         userScore += 1;
     }
   }
-};
-
-console.log("User1: " + userChoice);
-console.log("User2: " + computerChoice); 
-
-/* below code gets infinite loop....
-while (userScore || computerScore < 3) {
-console.log(compare(userChoice, computerChoice));
-console.log(userScore, computerScore);
 }
-*/
+
+while (userScore < 3 && computerScore < 3) {
+  console.log(compare(userChoice, computerChoice));
+  console.log("User1: " + userChoice);
+  console.log("User2: " + computerChoice); 
+  console.log(userScore, computerScore);
+}
+

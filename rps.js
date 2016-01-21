39b9da29b82127912e8361bf08f745d79416b3c2
TzolkinB <krbell4@gmail.com>
2016-01-20 20:23:23 -0500

    While loop is infinite

--- rps.js
+++ rps.js
@@ -1,5 +1,9 @@
+
 var userChoice = parseInt(Math.random()*10)%3;
 var computerChoice = parseInt(Math.random()*10)%3;
+
+var userScore = 0
+var computerScore = 0
 
 if (userChoice = 0) {
   	userChoice = "rock";
@@ -21,9 +25,6 @@ else {
     computerChoice = "scissors";
 } 
 
-console.log("User1: " + userChoice);
-console.log("User2: " + computerChoice); 
-
 var compare = function(choice1, choice2) {
     
   if (choice1 === choice2) {
@@ -33,30 +34,42 @@ var compare = function(choice1, choice2) {
   else if (choice1 === "rock") {
     if (choice2 === "scissors") {
       return "rock wins";
+      userScore += 1;
     }  
     else { 
         return "paper wins";
+        computerScore += 1;
     }
   }
     
   else if(choice1 === "paper") {
     if (choice2 === "rock") {
        return "paper wins";
+       userScore += 1;
     }
     else { 
         return "scissors wins";
-    }    
+    }   computerScore += 1; 
   }
     
   else if(choice1 === "scissors") {
     if (choice2 === "rock"){ 
         return "rock wins";
+        computerScore += 1;
     }
     else {
         return "scissors wins";
+        userScore += 1;
     }
   }
 };
 
-console.log(compare(userChoice, computerChoice))
+console.log("User1: " + userChoice);
+console.log("User2: " + computerChoice); 
 
+/* below code gets infinite loop....
+while (userScore || computerScore < 3) {
+console.log(compare(userChoice, computerChoice));
+console.log(userScore, computerScore);
+}
+*/

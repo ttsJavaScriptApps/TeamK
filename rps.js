f9ff0b95960a0c6604ae20d682ef87413905107f
Kimberly Bell <kimberly.bell@tma1.com>
2016-01-21 15:22:57 -0500

    loop works

--- rps.js
+++ rps.js
@@ -1,71 +1,70 @@
+var userChoice;
+var computerChoice;
 var userScore = 0
 var computerScore = 0
 
-var userChoice = parseInt(Math.random()*10)%3;
-var computerChoice = parseInt(Math.random()*10)%3;
-  
-  if (userChoice = 0) {
-      userChoice = "rock";
-  } 
-  else if(userChoice = 1) {
-      userChoice = "paper";
-  } 
-  else {
-      userChoice = "scissors";
-  } 
-
-  if (computerChoice == 0) {
-      computerChoice = "rock";
-  } 
-  else if(computerChoice == 1) {
-      computerChoice = "paper";
-  } 
-  else {
-      computerChoice = "scissors";
-  } 
-
-var compare = function(choice1, choice2) {
-      
-  if (choice1 === choice2) {
-      return "The result is a tie!";
-  }
-      
-  else if (choice1 === "rock") {
-    if (choice2 === "scissors") {
-      return "rock wins";
-      userScore += 1;
-    }  
-    else { 
-      return "paper wins";
-      computerScore += 1; 
-    }
-  }   
-  else if(choice1 === "paper") {
-    if (choice2 === "rock") {
-       return "paper wins";
-       userScore += 1;
-    }
-    else { 
-        return "scissors wins";
-        computerScore += 1; 
-    }
-  }    
-  else if(choice1 === "scissors") {
-    if (choice2 === "rock"){ 
-        return "rock wins";
-        computerScore += 1;
-    }
-    else {
-        return "scissors wins";
-        userScore += 1;
-    }
-  }
-}
-
 while (userScore < 3 && computerScore < 3) {
-  console.log(compare(userChoice, computerChoice));
-  console.log("User1: " + userChoice);
-  console.log("User2: " + computerChoice); 
-  console.log(userScore, computerScore);
-}
 
+  userChoice = parseInt(Math.random()*10)%3;
+  computerChoice = parseInt(Math.random()*10)%3;
+
+    if (userChoice = 0) {
+        userChoice = "rock";
+    } 
+    else if(userChoice = 1) {
+        userChoice = "paper";
+    } 
+    else {
+        userChoice = "scissors";
+    } 
+
+    if (computerChoice == 0) {
+        computerChoice = "rock";
+    } 
+    else if(computerChoice == 1) {
+        computerChoice = "paper";
+    } 
+    else {
+        computerChoice = "scissors";
+    } 
+
+    console.log("Player1: " + userChoice);
+    console.log("Player2: " + computerChoice); 
+        
+    if (userChoice === computerChoice) {
+        console.log("The result is a tie!");
+    }
+        
+    else if (userChoice === "rock") {
+      if (computerChoice === "scissors") {
+        console.log("rock wins");
+        userScore += 1;
+      }  
+      else { 
+        console.log("paper wins");
+        computerScore += 1; 
+      }
+    }   
+    else if(userChoice === "paper") {
+      if (computerChoice === "rock") {
+         console.log("paper wins");
+         userScore += 1;
+      }
+      else { 
+          console.log("scissors wins");
+          computerScore += 1; 
+      }
+    }    
+    else if(userChoice === "scissors") {
+      if (computerChoice === "rock"){ 
+        console.log("rock wins");
+          computerScore += 1;
+      }
+      else {
+          console.log("scissors wins");
+          userScore += 1;
+      }
+    }
+
+    console.log(userScore, computerScore);
+}

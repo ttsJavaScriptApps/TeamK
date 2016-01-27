ac6b6265a6e7d06852830d9ac658e0234eba153c
TzolkinB <krbell4@gmail.com>
2016-01-27 18:42:33 -0500

    RPS game using Functions

---
+++ rpsFunctions.js
@@ -0,0 +1,27 @@
+var hands = ['rock', 'paper', 'scissors'];
+
+function getHand() {
+	return hands[parseInt(Math.random()*10)%3];
+}
+
+var person1 = {
+	name: 'sam', 
+	hand: getHand
+};
+
+var person2 = {
+	name: 'ted', 
+	hand: getHand
+};
+
+function playRound(person1, person2) {
+	if (person1.hand() === person2.hand()) {
+	console.log("tie")
+	}
+	else if (person1.hand() === 0 && person2.hand() === 1)
+		console.log(person2 + " wins")
+
+
+}
+
+console.log(playRound());

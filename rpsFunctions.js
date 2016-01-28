785f045b018e26f2e68c2110e136cd3ab8f9f3a3
Kim <TzolkinB@users.noreply.github.com>
2016-01-27 22:21:06 -0500

    Everything works. Wrong order in console.

--- rpsFunctions.js
+++ rpsFunctions.js
@@ -1,28 +1,42 @@
 //Rock Paper Scissors game using functions
 var hands = ['rock', 'paper', 'scissors'];
 
+var player1Score = 0;
+var player2Score = 0;
+
 function getHand() {
 	return hands[parseInt(Math.random()*10)%3];
 }
 
-var person1 = {
-	name: 'sam', 
-	hand: getHand
-};
+function playRound(player1, player2) {
+  var player1 = { name: 'Katy',
+  hand: getHand() };
+  var player2 = { name: 'Kim',
+  hand: getHand() };
 
-var person2 = {
-	name: 'ted', 
-	hand: getHand
-};
-
-function playRound(person1, person2) {
-	if (person1.hand() === person2.hand()) {
-	console.log("tie")
+  	if (player1.hand === player2.hand) {
+  		console.log("It's a tie!")
+  	}
+  	else if ((player1.hand === 'rock' && player2.hand === 'scissors') || 
+  		(player1.hand === 'scissors' && player2.hand === 'paper') ||
+  		(player1.hand === 'paper' && player2.hand === 'rock')) {
+  		player1Score += 1;
+		console.log(player1.name + " wins this round")
 	}
-	else if (person1.hand() === 0 && person2.hand() === 1)
-		console.log(person2 + " wins")
+	else {
+		console.log(player2Score += 1)
+		console.log(player2.name + " wins this round")
+	}
+	return (player1.name + ': ' + player1.hand + " vs " + player2.name + ': ' + player2.hand);
 
+  };
 
-}
-
+while (player1Score < 3 && player2Score < 3) {
+    if (player1Score == 3)
+    	console.log("Katy wins the game!!")
+  //    console.log(player1.name + " wins the game!!"); 
+//player1.name not defined bc it is in playRound function
+    else
+       console.log("Kim wins the game!!");
 console.log(playRound());
+}
